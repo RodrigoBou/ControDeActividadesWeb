efd82e551b86482b2979134a25c6cee5cdd04a73
Rodrigo Alfonso Castro BOu <racb1994@gmail.com>
2016-06-11 18:24:47 -0600

    Conexion y control de servicios

---
+++ app/src/main/java/pdm/controdeactividadesweb/ClasesModelo/Reserva.java
@@ -0,0 +1,45 @@
+package pdm.controdeactividadesweb.ClasesModelo;
+/**
+ * Created by kelly on 23/04/2016.
+ * and
+ * Modified by Yohalmo
+ */
+public class Reserva {
+    private int id_reserva;
+    private int grupo_materia;
+    private int recurso;
+
+    public Reserva() {
+    }
+
+
+    public Reserva(int id_reserva, int grupo, int recurso) {
+        this.id_reserva = id_reserva;
+        this.grupo_materia = grupo;
+        this.recurso = recurso;
+    }
+
+    public int getIdReserva() {
+        return id_reserva;
+    }
+
+    public void setIdReserva(int idReserva) {
+        this.id_reserva = id_reserva;
+    }
+
+    public int getGrupo() {
+        return grupo_materia;
+    }
+
+    public void setGrupo(int grupo) {
+        this.grupo_materia = grupo;
+    }
+
+    public int getRecurso() {
+        return recurso;
+    }
+
+    public void setRecurso(int recurso) {
+        this.recurso = recurso;
+    }
+}
---
+++ app/src/main/java/pdm/controdeactividadesweb/Conexion.java
@@ -0,0 +1,24 @@
+package pdm.controdeactividadesweb;
+
+/**
+ * Created by Rodrigo on 6/11/2016.
+ */
+
+
+public class Conexion {
+    //Cambie estos Parametos segun su PC
+    public String URLLocal = "localhost";
+    public String URLServerLocal = "http://10.10.60.3";
+    public String URLServerPublico = "http://138.255.154.43";
+    public Conexion() {}
+
+    public String getURLServerLocal() {
+        return URLServerLocal;
+    }
+    public String getURLServerPublico() {
+        return URLServerPublico;
+    }
+    public String getURLLocal() {
+        return URLLocal;
+    }
+}
